cbc737ad3f5b209a7885bab0846c6c4abc691427
Filip Andrei Muresan <filip_andry@yahoo.it>
2020-09-24 09:16:52 +0200

    FIX interfaces export

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,7 @@
-import { IPage } from "./IPage";
-import { IWindowSize } from "./IWindowSize";
-import { ICardProps } from "./ICardProps";
+import { IPage as _IPage } from "./IPage";
+import { IWindowSize as _IWindowSize } from "./IWindowSize";
+import { ICardProps as _ICardProps } from "./ICardProps";
 
-export { IPage, IWindowSize, ICardProps };
+export type IPage = _IPage;
+export type IWindowSize = _IWindowSize;
+export type ICardProps = _ICardProps;
